95008d75295f33c99328c6eb6e0dbc2d1631793c
tvelic1 <92871186+tvelic1@users.noreply.github.com>
2024-08-04 01:52:11 +0200

    Update popupUtils.js

--- src/popupUtils.js
+++ src/popupUtils.js
@@ -1,6 +1,6 @@
-// popupUtils.js
 
-// Function to open a popup
+
+
 export const openPopup = (link) => {
     const url = link;
     const popupWindow = window.open(url, 'popup', 'width=600,height=400');
@@ -10,4 +10,4 @@ export const openPopup = (link) => {
         alert('Popup blocked. Please allow popups for this website.');
     }};
 
-// You can add more utility functions related to popups or other UI interactions here if needed
+
